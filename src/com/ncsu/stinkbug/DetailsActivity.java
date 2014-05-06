e3f4a9d2cad8d0eb1cc31115aa3e9a18395770d3
iyouq <i.uq@outlook.com>
2014-05-06 20:06:17 +0530

    - added additional text at the bottomg of the page ,in Summary.html - resolved bug in 'Your damage' calculation in Threshold Calculator

--- src/com/ncsu/stinkbug/DetailsActivity.java
+++ src/com/ncsu/stinkbug/DetailsActivity.java
@@ -1,30 +1,31 @@
 package com.ncsu.stinkbug;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.viewpagerindicator.CirclePageIndicator;
 
 public class DetailsActivity extends FragmentActivity implements OnPageChangeListener {
+
+	private Uri fileUri;
+	private MyUtil mUtil;
 	private TextView mTextFooter;
 	private int mActiveFrag;
 	private String[] footer = { "STINK BUG INTRODUCTION", "STINK BUG PESTS OF COTTON", "STINK BUG DAMAGE SYMPTOMS",
 			"STINK BUG SCOUTING STEPS", "STINK BUG DYNAMIC THRESHOLD", "STINK BUG DECISION AID CARD",
 			"STINK BUG BOLL SIZER", "STINK BUG THRESHOLD CALCULATOR", "STINK BUG SUMMARY" };
-
-	/*
-	 * private String[] footer = { "Stink Bug INTRODUCTION",
-	 * "Stink Bug PESTS OF COTTON", "Stink Bug DAMAGE SYMPTOMS",
-	 * "Stink Bug SCOUTING STEPS", "Stink Bug DYNAMIC THRESHOLD",
-	 * "Stink Bug DECISION AID CARD", "Stink Bug BOLL SIZER",
-	 * "Stink Bug SUMMARY" };
-	 */
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,8 @@ public class DetailsActivity extends FragmentActivity implements OnPageChangeLis
 
 		mTextFooter = (TextView) findViewById(R.id.footerText);
 
+		mUtil = new MyUtil(this);
+
 		// Set the pager with an adapter
 		ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
 		// pager.setAdapter(new TestPagerAdapter(this));
@@ -43,7 +46,7 @@ public class DetailsActivity extends FragmentActivity implements OnPageChangeLis
 		mActiveFrag = getIntent().getExtras().getInt("activeFrag", 0);
 		updateFooter(footer[mActiveFrag]);
 
-		Log.d("DetailsActivity", "active fragment:" + mActiveFrag);
+		//		Log.d("DetailsActivity", "active fragment:" + mActiveFrag);
 
 		CirclePageIndicator circleIndicator = (CirclePageIndicator) findViewById(R.id.titles);
 		circleIndicator.setViewPager(pager);
@@ -53,15 +56,62 @@ public class DetailsActivity extends FragmentActivity implements OnPageChangeLis
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_photo:
+			openCamera();
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void openCamera() {
+		Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+		// create a file to save the image
+		fileUri = MyUtil.getOutputMediaFileUri(MyUtil.MEDIA_TYPE_IMAGE);
+
+		// set the image file name
+		camIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+		startActivityForResult(camIntent, MyUtil.CAM_REQUEST_CODE);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == MyUtil.CAM_REQUEST_CODE) {
+			if (resultCode == RESULT_OK) {
+				// Image captured and saved to fileUri specified in the Intent
+
+				// Toast.makeText(this, "Image saved to:\n" + data.getData(),
+				// Toast.LENGTH_LONG).show();
+				mUtil.sendEmail(fileUri);
+
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the image capture
+			} else {
+				// Image capture failed, advise user
+				Toast.makeText(this, "Image capture failed.", Toast.LENGTH_LONG).show();
+			}
+		}
+
 	}
 
 	@Override
 	public void onAttachFragment(Fragment fragment) {
 		super.onAttachFragment(fragment);
-		Log.d("Details", fragment.getTag() + "");
+		// Log.d("Details", fragment.getTag() + "");
 	}
 
 	@Override
@@ -79,7 +129,7 @@ public class DetailsActivity extends FragmentActivity implements OnPageChangeLis
 	@Override
 	public void onPageSelected(int arg0) {
 		// TODO Auto-generated method stub
-		Log.d("PagerListeber", "page: " + arg0 + " title: " + footer[arg0]);
+		// Log.d("PagerListeber", "page: " + arg0 + " title: " + footer[arg0]);
 		// getActionBar().setTitle(footer[arg0]);
 		updateFooter(footer[arg0]);
 	}
@@ -87,4 +137,5 @@ public class DetailsActivity extends FragmentActivity implements OnPageChangeLis
 	private void updateFooter(String string) {
 		mTextFooter.setText(string);
 	}
+
 }
--- src/com/ncsu/stinkbug/frags/EighthFragment.java
+++ src/com/ncsu/stinkbug/frags/EighthFragment.java
@@ -1,15 +1,27 @@
 package com.ncsu.stinkbug.frags;
 
+import java.util.Hashtable;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 import com.ncsu.stinkbug.R;
 
-public class EighthFragment extends Fragment {
+public class EighthFragment extends Fragment /* implements OnEditorActionListener */{
 	private String mFile = "file:///android_asset/index.html";
+
+	private EditText mtfWeekOfBloom, mtfNumBollsSampled, mtfDamagedBolls;
+	private TextView mlblTreatmentThreshold, mlblYourDamage;
+	private Hashtable<String, String> mTable = new Hashtable<String, String>();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -17,6 +29,53 @@ public class EighthFragment extends Fragment {
 
 		// WebView w = (WebView) v.findViewById(R.id.firstWebView);
 		// w.loadUrl(mFile);
+		mtfWeekOfBloom = (EditText) v.findViewById(R.id.tfWeekOfBloom);
+		mtfNumBollsSampled = (EditText) v.findViewById(R.id.tfNumBolls);
+		mtfDamagedBolls = (EditText) v.findViewById(R.id.tfDamagedBolls);
+
+		mtfWeekOfBloom.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				int result = actionId & EditorInfo.IME_MASK_ACTION;
+
+				switch (result) {
+
+				case EditorInfo.IME_ACTION_NEXT:
+					setTreatmentThreshold();
+					break;
+				}
+				return false;
+			}
+		});
+		mtfDamagedBolls.setOnEditorActionListener(new OnEditorActionListener() {
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				int result = actionId & EditorInfo.IME_MASK_ACTION;
+
+				switch (result) {
+				case EditorInfo.IME_ACTION_DONE:
+				case EditorInfo.IME_ACTION_NEXT:
+					calculateYourDamage();
+
+					break;
+				}
+				return false;
+			}
+		});
+
+		mTable.put("1", "50%");
+		mTable.put("2", "30%");
+		mTable.put("3", "10%");
+		mTable.put("4", "10%");
+		mTable.put("5", "10%");
+		mTable.put("6", "20%");
+		mTable.put("7", "30%");
+		mTable.put("8", "50%");
+
+		mlblTreatmentThreshold = (TextView) v.findViewById(R.id.tvTreatmentThreshold);
+		mlblYourDamage = (TextView) v.findViewById(R.id.tvYourDamage);
 
 		return v;
 	}
@@ -30,5 +89,46 @@ public class EighthFragment extends Fragment {
 		f.setArguments(b);
 
 		return f;
+	}
+
+	private void calculateYourDamage() {
+		Log.e("EighthFrag", "calculateYourDamage");
+		/**
+		 * formula: Your damage = (Damaged bolls / num bolls sampled) * 100
+		 */
+		try {
+			float valDamagedBolls = Float.parseFloat(mtfDamagedBolls.getText().toString());
+			float valNumBollsSample = Float.parseFloat(mtfNumBollsSampled.getText().toString());
+
+			int result = (int) ((valDamagedBolls / valNumBollsSample) * 100.00);
+			mlblYourDamage.setText(result + "%");
+			Log.d("EighthFrag", String.format("Num bools: %s, Damaged Bolls: %s, result: %d", mtfNumBollsSampled
+					.getText().toString(), mtfDamagedBolls.getText().toString(), result));
+		} catch (Exception e) {
+			Log.e("Eighth Frag", "Exception: " + e.getMessage());
+		}
+	}
+
+	/*
+	 * @Override public boolean onEditorAction(TextView v, int actionId,
+	 * KeyEvent event) { int result = actionId & EditorInfo.IME_MASK_ACTION;
+	 * 
+	 * switch (result) {
+	 * 
+	 * case EditorInfo.IME_ACTION_NEXT: setTreatmentThreshold(); break; } return
+	 * false; }
+	 */
+
+	private void setTreatmentThreshold() {
+		String text = "---";
+
+		String key = mtfWeekOfBloom.getText().toString();
+
+		if (mTable.containsKey(key)) {
+			text = mTable.get(key);
+		}
+
+		mlblTreatmentThreshold.setText(text);
+
 	}
 }
